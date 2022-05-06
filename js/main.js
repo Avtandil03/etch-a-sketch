4f18ce92bda7e0adf328ee981f713033109f7507
Avtandil <avtandilqazxswedc@gmail.com>
2022-05-06 16:08:49 +0300

    Завершённый проект без подглядываний

--- js/main.js
+++ js/main.js
@@ -7,23 +7,59 @@ window.addEventListener("DOMContentLoaded", () => {
         sizeValue = document.getElementById("sizeValue"),
         sizeSlider = document.getElementById("sizeSlider"),
         grid = document.getElementById("grid"),
-        repeatNumber = sizeSlider.value;
+        settingDiv = document.querySelector(".settings"),
+        paintMode = "color",
+        color = colorInput.value;
+
+    createDivs(sizeSlider.value);
+
+    settingDiv.addEventListener("click", (e) => {
+
+        if (e.target == colorBtn || e.target == rainbownBtn || e.target == eraser) {
+            colorBtn.classList.remove("active");
+            rainbownBtn.classList.remove("active");
+            eraser.classList.remove("active");
+            e.target.classList.add("active")
+        }else if (e.target == clearBtn)
+            createDivs(sizeSlider.value)
+
+
+
+    });
 
     sizeSlider.addEventListener("change", () => {
-        sizeValue.innerHTML = `${sizeSlider.value} X ${sizeSlider.value}`
         createDivs(sizeSlider.value);
-    })
+    });
 
     function createDivs(howMuchElem) {
-
-        grid.style.setProperty('--repeatNumber', sizeSlider.value);
+        sizeValue.innerHTML = `${sizeSlider.value} X ${sizeSlider.value}`;
         grid.replaceChildren();
+        grid.style.setProperty("--repeatNumber", howMuchElem);
 
-        for (let i = 0; i < (howMuchElem * howMuchElem); i++) {
-            let girdElem = document.createElement("div");
-            girdElem.classList.add("girdElem");
-            grid.appendChild(girdElem);
+        for (let i = 0; i < howMuchElem * howMuchElem; i++) {
+            let gridElem = document.createElement("div");
+            gridElem.classList.add("gridElem");
+            grid.appendChild(gridElem);
         }
     }
 
-})
+    grid.addEventListener("mousemove", (e) => {
+        let elem = e.target;
+        let mode = document.querySelector(".active");
+
+        switch (mode) {
+            case colorBtn:color = colorInput.value;
+                break;
+            case rainbownBtn: color = 'rgb(' + 
+            (Math.floor(Math.random() * 256)) + 
+            ',' + (Math.floor(Math.random() * 256)) + 
+            ',' + (Math.floor(Math.random() * 256)) + ')';
+                break;
+            case eraser: color = "white"
+                
+                
+        }
+        elem.style.backgroundColor = color;
+        console.log(elem.style.backgroundColor);
+    });
+});
